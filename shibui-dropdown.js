3639bb51fa41c53cf5441e42372ec56e04649eae
michael <me@michaelgreen.net>
2018-07-10 12:53:10 -0700

    Updated to Polymer 3🌹🌹🌹🌹🌹🌹🌹🌹

--- shibui-dropdown.js
+++ shibui-dropdown.js
@@ -18,6 +18,7 @@ Custom property | Description | Default
 `--shibui-dropdown-color` | A property that controls the color of the dropdown items | `black`
 `--shibui-dropdown-content` | A mixin that is applied to the dropdown | `{}`
 
+<<<<<<< HEAD:shibui-dropdown.js
 @demo demo/shibui-dropdown.html
 */
 /*
@@ -33,6 +34,13 @@ $_documentContainer.setAttribute('style', 'display: none;');
 
 $_documentContainer.innerHTML = `<dom-module id="shibui-dropdown">
   <template strip-whitespace="">
+=======
+@demo demo/shibui-dropdown.html Demo
+-->
+
+<dom-module id="shibui-dropdown">
+  <template strip-whitespace>
+>>>>>>> 2ce55b71532b3e392f228a2b765526248532babd:shibui-dropdown.html
     <style>
       :host {
         background: var(--shibui-dropdown-background, white);
@@ -40,11 +48,12 @@ $_documentContainer.innerHTML = `<dom-module id="shibui-dropdown">
 
         display: inline-block;
         font-size: 14px;
-        position: absolute;
+        position: fixed;
         width: auto;
         border-radius: 3px;
         box-shadow: 0 3px 20px rgba(89, 105, 129, 0.3), 0 1px 2px rgba(0, 0, 0, 0.05), 0 0 0 1px rgba(89, 105, 129, .1);
         white-space: nowrap;
+        overflow: hidden;
 
         opacity: 0;
         pointer-events: none;
@@ -91,6 +100,7 @@ $_documentContainer.innerHTML = `<dom-module id="shibui-dropdown">
     <slot id="content"></slot>
   </template>
 
+<<<<<<< HEAD:shibui-dropdown.js
   
 </dom-module>`;
 
@@ -161,3 +171,124 @@ Polymer({
     }
   }
 });
+=======
+  <script>
+    Polymer({
+      is: 'shibui-dropdown',
+
+      properties: {
+        /**
+         * Determines whether the dropdown is opened or closed
+         */
+        opened: {
+          type: Boolean,
+          value: false,
+          notify: true,
+          reflectToAttribute: true,
+          observer: '_openedChanged'
+        },
+
+        /**
+         * Whether to align the dropdown to the 'right', 'center', or 'left' of its 'target'
+         */
+        alignment: {
+          type: String,
+          value: 'right',
+          observer: 'resize'
+        },
+
+        /**
+         * The target to attach the dropdown to.
+         */
+        target: Element,
+
+        /**
+         * The buffer from the edge of the page
+         */
+        buffer: {
+          type: Number,
+          value: 10,
+          observer: '_bufferUpdated'
+        }
+      },
+
+      attached: function() {
+        this.toggle = this.toggle.bind(this);
+        this.open = this.open.bind(this);
+        this.close = this.close.bind(this);
+        this.resize = this.resize.bind(this);
+      },
+
+      detached: function() {
+        document.removeEventListener('click', this.close);
+      },
+
+      /**
+       * Toggles the opened state of the dropdown
+       */
+      toggle: function() {
+        this.set('opened', !this.opened);
+      },
+
+      /**
+       * Opens the dropdown
+       */
+      open: function() {
+        this.set('opened', true);
+      },
+
+      /**
+       * Closes the dropdown
+       */
+      close: function() {
+        this.set('opened', false);
+      },
+
+      /**
+       * Forces the dropdown to resize
+       */
+      resize: function() {
+        var target = this.target || this.parentElement;
+
+        if (!this.opened || !target) return;
+
+        var rect = target.getBoundingClientRect();
+        var left = rect.x;
+        if (this.alignment === 'right') {
+          left += rect.width - this.clientWidth;
+        } else if (this.alignment === 'center') {
+          left += (rect.width / 2) - (this.clientWidth / 2);
+        }
+
+        left = Math.min(window.innerWidth - this.clientWidth - this.buffer, left);
+        left = Math.max(this.buffer, left);
+
+        this.style.left = left + 'px';
+        this.style.top = rect.y + rect.height + this.buffer + 'px';
+      },
+
+      _openedChanged: function(opened) {
+        if (opened) {
+          this.resize();
+          Polymer.RenderStatus.afterNextRender(this, function() {
+            document.addEventListener('click', this.close);
+            window.addEventListener('resize', this.resize, {
+              passive: true
+            });
+            window.addEventListener('scroll', this.resize, {
+              passive: true
+            });
+          });
+        } else {
+          document.removeEventListener('click', this.close);
+        }
+      },
+
+      _bufferUpdated: function(buffer) {
+        this.style.maxWidth = 'calc(100% - ' + (buffer * 2) + 'px)';
+        this.resize();
+      }
+    });
+  </script>
+</dom-module>
+>>>>>>> 2ce55b71532b3e392f228a2b765526248532babd:shibui-dropdown.html
